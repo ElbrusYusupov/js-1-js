57a996e72f7c8eebe209907ec117faa5efb21912
ElbrusYusupov <yusupoffup@gmail.com>
2021-06-13 19:49:51 +0300

    123

---
+++ JS/m1.js
@@ -0,0 +1,14 @@
+const x = 5;
+const y = 10;
+const z = 5;
+
+console.log(x>z); // false
+console.log(z<y); // true
+console.log(x>y); // false
+console.log(z<=x); // true
+console.log(y===x); // false
+console.log(z!==y); // true
+console.log(y!==x); // true
+console.log(z===y); // false
+// Сделать их целыми, умножив на 10, сложить, а результат разделить на 10
+console.log((0.1 * 10 + 0.2 * 10) / 10);
