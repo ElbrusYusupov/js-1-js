3b1bd8b6aaa2f1609c71dd9b4e39c1b4c0096f4a
ElbrusYusupov <yusupoffup@gmail.com>
2021-07-06 22:01:23 +0300

    gdgdf

--- JS/m4.js
+++ JS/m4.js
@@ -187,16 +187,253 @@
 // counterB.increment()
 // counterB.increment()
 
-const makeDish = function (shefName, dish) {
-    console.log(`${shefName} is cooking ${dish}`);
+// const makeDish = function (shefName, dish) {
+//     console.log(`${shefName} is cooking ${dish}`);
 
-}
+// }
 
-makeDish('Mango', 'apple pie');
-makeDish('Mango', 'fish');
-makeDish('Mango', 'beef stew');
+// makeDish('Mango', 'apple pie');
+// makeDish('Mango', 'fish');
+// makeDish('Mango', 'beef stew');
 
 
-makeDish('Poly', 'muffins');
-makeDish('Poly', 'pork chops');
-makeDish('Poly', 'roast beef')
+// makeDish('Poly', 'muffins');
+// makeDish('Poly', 'pork chops');
+// makeDish('Poly', 'roast beef')
+
+// const makeShef = function (name) {
+//   /*
+//   The name parameter is local variable for the makeShef function.
+//   This means that it will be available to the makeDish function via a closure
+//   */
+  
+//   return function makeDish(dish) {
+//     console.log(`${name} is cooking ${dish}`);
+//   }
+// }
+
+// const mango = makeShef('Mango');
+// mango('apple pie');
+// mango('beef stew');
+
+// const poly = makeShef('Poly');
+// poly('pancakes');
+// poly('eggs and bacon');
+
+
+//// $$$$$$$$$ Execution context (this)
+
+/* Petya runs fast because Petya is trying to catch the train
+*/
+// const petya = {
+//   name: 'Petya',
+//   showName() {
+//     console.log(petya.name);
+//   },
+// };
+
+// petya.showName()
+
+/* Petya runs fast because he (this) is trying to catch the train
+*/
+// const petya = {
+//   name: 'Petya',
+//   showName() {
+//     console.log(this.name);
+//   },
+
+// }
+// petya.showName()
+
+//// $$$$$$$$ Rules for defining 'This'
+
+// //// $$$$ 'this in the global scope
+
+// function fn() {
+//   console.log(this);
+// }
+// fn()
+
+
+// //// $$$$ 'this' in the object method
+
+// const petya = {
+//   name: 'Petya',
+//   showThis() {
+//     console.log(this);
+//   },
+//   showName() {
+//     console.log(this.name);
+//   },
+// };
+// petya.showThis()
+// petya.showName()
+
+// function showThis() {
+//   console.log('This in showThis ', this);
+// }
+
+// //// Calling in the global context
+// showThis();
+
+// const user = { name: 'Mango' };
+
+// //// We write the link to the function into the object property.
+// // Note that this is not a call - not ()
+
+// user.showContext = showThis;
+
+// /* We call the function in the context of the object, in context which is being called, not the global object
+// */
+
+// user.showContext();
+
+
+//// $$$ this in callback function
+
+// const hotel = {
+//   name: 'Resort Hotel',
+//   showThis() {
+//     console.log(this);
+//   },
+// };
+
+// const fn = function (callback) {
+//   /* When fn is called, callback will be a link to the showThis function of the hotel object.
+//   * Its call takes place in a global context, she doesn't know anything about the hotel.
+//  * Accordingly, this will not refer to hotel.
+//  */
+  
+//   callback()
+// }
+
+// //// A reference to the function is passed, not its call.
+
+// fn(hotel.showThis)
+
+// /////// $$$$$$ 'this' in arrow function
+
+// const showThis = () => {
+//   console.log('This is showThis: ', this);
+// }
+
+// showThis();
+
+
+// const user = { name: 'Mango' };
+// user.showContext = showThis;
+
+// user.showContext()
+
+// const hotel = {
+//   name: 'Resort Hotel',
+//   showThis() {
+//     const fn = () => {
+
+//       /* The arrows remember the context during the announcement, from parent scope */
+//       console.log('This in fn: ', this);
+//     };
+//     fn();
+//     console.log('This in shoeThis: ', this);
+//   },
+// };
+// hotel.showThis()
+
+
+//// $$$ if you convert this code to ES5, you get the following
+
+// const hotel = {
+//   name: 'Resort Hotel',
+//   showThis: function showThis() {
+//     // the context for the arrow is preserved and passed from outside scope
+
+//     const context = this;
+
+//     const fn = function fn() {
+//       // And here it is used
+
+//       console.log('this is fn: ', context);
+//     };
+
+//     fn();
+//     console.log('this is showThis: ', this);
+//   },
+// };
+
+// hotel.showThis()
+
+////$$$$ Function methods call, apply, bind
+
+// const greet = function () {
+//   return `Wellcome to ${this.name}!`;
+
+// };
+
+// const hotel = { name: "Resort Hotel" };
+
+// console.log(greet.call(hotel));
+// console.log(greet.apply(hotel));
+
+
+//// $$$ call and arguments
+// const greet = function (guest, stars) {
+//   return `${guest}, wellcome to ${stars}-stars ${this.name}`;
+
+// };
+
+
+// const hotel = { name: 'Resort Hotel' };
+// const motel = { name: 'Sunlight Motel' };
+
+// console.log(greet.call(hotel, 'Mango', 5));
+// console.log(greet.call(motel, 'Poly', 4));
+
+
+//// $$$$$$ apply and arguments
+
+// const greet = function (guest, stars) {
+//   return `${guest}, wellcome to ${stars}-stars ${this.name}`;
+
+// };
+// const hotel = { name: 'Resort Hotel' };
+// const motel = { name: "Sunlight Motel" }
+
+// console.log(greet.apply(hotel, ['Mango', 5]));
+// console.log(greet.apply(motel, ['Poly', 4]));
+
+//// $$$$ bind 
+
+// const greet = function (guest) {
+//   return `${guest} welcome to ${this.name}!`;
+
+// };
+
+// const hotel = { name: 'Resort Hotel' };
+// const hotelGreeter = greet.bind(hotel, 'Mango');
+// hotelGreeter()
+
+// // const greet = function (guest) {
+// //   return `${guest}, welcome to ${this.name}!`;
+// // };
+
+// // const hotel = { name: 'Resort Hotel' };
+
+// // const hotelGreeter = greet.bind(hotel, 'Mango');
+
+// // hotelGreeter(); // "Mango, welcome to Resort Hotel!"
+
+
+const hotel = {
+  name: 'Resort Hotel',
+  showThis() {
+    console.log(this);
+  },
+};
+
+const fn = function (callback) {
+  callback()
+};
+
+// Pass the copy of the showThis method with the context bound to the hotel
+
+fn(hotel.showThis.bind(hotel))
